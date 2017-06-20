e4b1f937bba0463694d43ec9fe995691448b918b
johnsaugy <john.saugy@gmail.com>
2017-06-20 00:34:24 -0400

    Add files via upload

---
+++ trivia/assets/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function (){
+
+//Questions (10)
+var questions = [
+	{
+		id: 1,
+		question: "What is the capital of New York?",
+		answers: ["Albany", "New York City", "Buffalo", "Montauk"],
+		correctAnswer: 1
+	},
+	{
+		id: 2,
+		question: "How many states are there in the U.S.A?",
+		answers: ["49", "50", "51", "52"],
+		correctAnswer: 2
+	},
+	{
+		id: 3,
+		question: "Who let the dogs out?",
+		answers: ["Baha Men", "Outkast", "I don't know", "The pound"],
+		correctAnswer: 3
+	},
+	{
+		id: 4,
+		question: "What is the name of the newest team in the National Hockey League?",
+		answers: ["Sacramento Warriors", "Kansas City Whirlwind", "Jacksonville Jazz", "Las Vegas Knights"],
+		correctAnswer: 4
+	},
+	{
+		id: 5,
+		question: "What is considered the powerhouse of the cell?",
+		answers: ["Nucleus", "Ribosome", "Cell Membrane", "Mitochondria"],
+		correctAnswer: 4
+	},
+	{
+		id: 6,
+		question: "Amazon recently acquired which company?",
+		answers: ["Trader Joes", "Safeway", "Whole Foods", "Lyft"],
+		correctAnswer: 3
+	},
+	{
+		id: 7,
+		question: "Which one of these ski resorts is located in Colorado?",
+		answers: ["Stowe", "Vail", "Gore", "Heavenly"],
+		correctAnswer: 2
+	},
+	{
+		id: 8,
+		question: "What is the name of Jon Snow's direwolf?",
+		answers: ["Ghost", "Snow", "Nymeria", "Grey Wind"],
+		correctAnswer: 1
+	},
+	{
+		id: 9,
+		question: "This actress plays Maeve in HBO's Westworld",
+		answers: ["Thandie Newton", "Evan Rachel Wood", "Sophie Turner", "Maise Williams"],
+		correctAnswer: 1
+	},
+	{
+		id: 10,
+		question: "In what year was the first iPod released?",
+		answers: ["2000", "2001", "2002", "2003"],
+		correctAnswer: 2
+	},
+]
